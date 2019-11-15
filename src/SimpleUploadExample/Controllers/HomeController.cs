ea38100e8e64d4991d68dda285377337d1dda786
reinaldo <reinaldo.coelho@gmail.com>
2019-11-14 23:16:23 -0300

    Simple HTML5 example.

---
+++ src/SimpleUploadExample/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SimpleUploadExample.Models;
+
+namespace SimpleUploadExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public void ActionResultUploadFile([FromForm] List<IFormFile> files)
+        {
+            try
+            {
+                foreach (var file in files)
+                {
+                    if (file.Length > 0)
+                    {
+                        Console.WriteLine($"O arquivo {file.FileName} chegou e tem mais de 0 bytes.");
+                        // string _FileName = Path.GetFileName(file.FileName);
+                        // string _path = Path.Combine(Server.MapPath("~/UploadedFiles"), _FileName);
+                        // file.SaveAs(_path);
+                    }
+                }
+                ViewBag.Message = "File Uploaded Successfully!!";
+            }
+            catch
+            {
+                ViewBag.Message = "File upload failed!!";
+            }
+        }
+    }
+}
